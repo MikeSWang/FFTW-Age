d87f8360e5deed6a53fd30e994009afa90d8666d
Mike S Wang <32841762+MikeSWang@users.noreply.github.com>
2024-06-29 01:44:14 +0100

    MAINT: Update syntax

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,12 +1,19 @@
+#!/usr/bin/env sh
+#
+# @file postCreate.sh
+# @author Mike S Wang
+#
+
 # Initialise Conda for each shell.
 conda init bash
 conda init zsh
 
 # Imitate `postStartCommand` using configuration files for each shell.
-# cat .devcontainer/postStart.sh >> $HOME/.bashrc
-# cat .devcontainer/postStart.sh >> $HOME/.zshrc
-echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> $HOME/.bashrc
-echo "alias welcome-notice='cat /usr/local/etc/vscode-dev-containers/first-run-notice.txt'" >> $HOME/.zshrc
+# cat .devcontainer/postStart.sh >> "$HOME"/.bashrc
+# cat .devcontainer/postStart.sh >> "$HOME"/.zshrc
+NOTICE_FILE="/usr/local/etc/vscode-dev-containers/first-run-notice.txt"
+echo "alias welcome-notice='cat ${NOTICE_FILE}'" >> "$HOME"/.bashrc
+echo "alias welcome-notice='cat ${NOTICE_FILE}'" >> "$HOME"/.zshrc
 
 # Fetch Git tags.
 git fetch --tags
